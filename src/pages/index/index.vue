8e82778c92593097743049d8eb515dfbce5c6a30
caipeng <caipenghsxr@gmail.com>
2019-08-14 18:04:16 +0800

    sample demo test succeful

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container" @click="clickHandle">
+    <div class="message">{{msg}}</div>
+    <!-- 使用 click-counter 组件 -->
+    <click-counter />
+  </div>
+</template>
+
+<script>
+// 导入 click-counter 组件
+import ClickCounter from "@/components/click-counter";
+
+export default {
+  // 声明在当前组件下使用 counter-click 组件
+  components: { ClickCounter },
+
+  data() {
+    return {
+      msg: "Hello"
+    };
+  },
+
+  methods: {
+    clickHandle() {
+      this.msg = "Clicked!!!!!!";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
+}
+</style>
